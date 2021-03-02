3059c3ea4ee861dcaa5c57458d9b4d58cd793029
Lucas-far <mizoguchia@hotmail.com>
2021-03-02 20:36:29 -0300

    .

--- rascunhos.py
+++ rascunhos.py
@@ -1,3 +1,51 @@
 
 
+"""
+the butter came from milk
+the butter comes from milk
+the butter will come from milk
 
+the butter was coming from milk
+the butter is coming from milk
+the butter will be coming from milk
+
+the butter had come from milk
+the butter has come from milk
+the butter will have come from milk
+
+the butter had been coming from milk
+the butter has been coming from milk
+the butter will have been coming from milk
+
+this butter is yellowish
+that butter was at the table
+these butters can be placed into the fridge
+those butters are expired
+
+the butter is constantly warm
+the butter is above the table
+"""
+
+personal_pronouns = 'pronomes pessoais'
+demonstrative_pronouns = 'pronomes demonstrativos'
+reflexive_pronouns = 'pronomes reflexivos'
+possessive_pronouns = 'pronomes possessivos'
+
+possessive_adjectives = 'adjetivos possessivos'
+
+adverbs_of_frequency = 'advérbios de frequência'
+adverbs_of_mode = 'advérbios de modo'
+adverbs = 'advérbios'
+
+adjectives = 'adjetivos'
+articles = 'artigos'
+
+conjunctions = 'conjunções'
+
+nouns = 'substantivos'
+
+can = 'can'
+have = 'have'
+do = 'do'
+need = 'need to'
+start = 'start + ing'
